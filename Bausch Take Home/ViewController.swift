ac3c303f423cef54b82cb5bcc77f8e809e76ce94
Tim Bausch <baus8650@gmail.com>
2022-01-05 06:41:58 -0800

    Initial Commit

---
+++ Bausch Take Home/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Bausch Take Home
+//
+//  Created by Tim Bausch on 1/5/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
